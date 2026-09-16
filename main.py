f88fb62b2b82ef7d1af7ccfbe895f59a17b289cb
johannesmakgetha <makgethamj@gmail.com>
2026-09-16 11:32:04 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+#imports
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QPushButton, QHBoxLayout,QVBoxLayout,QGridLayout
+
+
+
+# App settings
+app = QApplication([])
+main_windown = QWidget()
+main_windown.setWindowTitle("Calculator APP")
+main_windown.resize(250,300)
+
+
+# Widgets
+
+
+
+# Layouts
+
+
+
+# Events
+
+
+
+
+# Show/Run
+
+main_windown.show()
+app.exec()
