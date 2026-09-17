3e43d6d25ad05418adb0f0a23a75cbf0202fa11e
johannesmakgetha <makgethamj@gmail.com>
2026-09-17 05:27:02 +0200

    grid layout

--- main.py
+++ main.py
@@ -11,18 +11,54 @@ main_windown.setWindowTitle("Calculator APP")
 main_windown.resize(250,300)
 
 
-# Widgets
+# Widgets/ All objects
+
+text_box = QLineEdit()
+grid = QGridLayout()
+
+buttons = [
+    "7","8","9","/",
+    "4","5","6","*",
+    "1","2","3","+",
+    "0",".","=","-"
+           ]
+
+
+clear = QPushButton("Clear")
+delete = QPushButton("<")
+
+col = 0
+row = 0
+
+for text in buttons:
+    button = QPushButton(text)
+    #button_d.clicked
+    grid.addWidget(button,row,col)
+    col += 1
+
+    if col > 3:
+        col = 0
+        row += 1
+
+
 
 
 
 # Layouts
+master_layout = QVBoxLayout()
+master_layout.addWidget(text_box)
+master_layout.addLayout(grid)
 
+button_row = QHBoxLayout()
+button_row.addWidget(clear)
+button_row.addWidget(delete)
 
-
+master_layout.addLayout(button_row)
 # Events
 
 
 
+main_windown.setLayout(master_layout)
 
 # Show/Run
 
